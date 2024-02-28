b0d12f693a4698f81ff9c989f1f5747d61ce2bfb
masroor <masroor.1388@gmail.com>
2024-02-28 22:27:25 +0330

    پروژه حدث کلمه

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
- 
- 
+import ParentCompo from './ParentCompo';
 
 const App = ()=>{
-  
     return (
-      
-        <div className="container w-100 h-100 p-3">
-            <div className="row h-100 justify-content-center align-align-items-start">
-                <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
-                
-                </div>
-            </div>
+        <div>
+            <ParentCompo/>
         </div>
-        
     ) 
 }
 
